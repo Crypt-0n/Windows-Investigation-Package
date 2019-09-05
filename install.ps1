1569819a7c92d7946c09c2d58523bba77ea3b955
Julien LEQUEN <jlequen@crypt-0n.fr>
2019-09-05 16:09:06 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -118,5 +118,5 @@ DownloadELK
 DownloadC0-FF-EE
 Chocolatey
 
-
-#Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux 
+DownloadKali
